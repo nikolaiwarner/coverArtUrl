38d86ffe47fc403ba5fddec51e3c6e9ef8f65631
Nick Warner <nickwarner@gmail.com>
2020-08-07 22:44:02 -0400

    Use node-musicbrainz package that works in browsers

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
+const musicbrainz = require('musicbrainz')
 const fetch = require('node-fetch')
-const Nodebrainz = require('nodebrainz')
 
 function coverArtUrl({ artist, release }) {
   return new Promise((resolve, reject) => {
-    const nodebrainz = new Nodebrainz({
-      userAgent: 'coverArtUrl/1.0.2 ( https://github.com/nikolaiwarner/coverarturl )',
-    })
-    nodebrainz.search('release', { artist, release, limit: 1 }, async function (
-      err,
-      releases,
-    ) {
+    musicbrainz.searchReleases(release, { artist }, async function (err, releases) {
       if (err) reject(err)
-      if (releases && releases.releases.length) {
+      if (releases.length) {
         const response = await fetch(
-          `https://coverartarchive.org/release/${releases.releases[0].id}`,
+          `https://coverartarchive.org/release/${releases[0].id}`,
         )
         if (response.ok) {
           const coverArt = await response.json()
